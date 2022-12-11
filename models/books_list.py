b685ab2c06aaa658873a89bcdf0cc09391205407
Marcin <marcinegzmont@gmail.com>
2022-12-11 21:21:43 +0000

    added extensions

--- models/books_list.py
+++ models/books_list.py
@@ -1,7 +1,7 @@
 from models.book import Book
 
-book_1 = Book("Gift", "Vladimir Nabokov", "Foreign literature" )
-book_2 = Book("Selected Esseys", "George Orwell", "Esseys")
-book_3 = Book("Psycho-logical", "Dean Burnett", "Psychology") 
+book_1 = Book("Gift", "Vladimir Nabokov", "Foreign literature", False )
+book_2 = Book("Selected Esseys", "George Orwell", "Esseys", True)
+book_3 = Book("Psycho-logical", "Dean Burnett", "Psychology", False) 
 
 books = [book_1, book_2, book_3]
